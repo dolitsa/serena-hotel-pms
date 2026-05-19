8aafd796a5ae3a2ef2bdc21debe6ba96ff8a076f
dolitsa <mariadoli24@yahoo.gr>
2026-05-19 22:18:41 +0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'SERENA HOTEL PMS',
+  description: 'Hotel Management System',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="el">
+      <body>{children}</body>
+    </html>
+  )
+}
